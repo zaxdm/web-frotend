a97fe0915859d28eaf4791d5d5a87b20a64f133c
zaxdm <esteban.dioses@tecsup.edu.pe>
2026-02-20 11:29:57 -0500

    vistaproducto-actualizadoa

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -12,17 +12,28 @@ export class ProductService {
       { label: 'BROCAS DE RODILLO CÓNICO', link: '/productos/brocas-de-rodillo' }
 
     ],
-    title: 'Producto ejemplo',
-    subtitle: 'Subtítulo ejemplo',
+    title: 'RIDGEBACK™',
+    subtitle: 'Perforación de roca dura',
     descriptions: [
-      'Descripción principal del producto',
-      'Otra descripción del producto'
+      'Brocas giratorias para perforación de pozos en roca dura. Camisa especialmente diseñada para ayudar en la evacuación rápida de esquejes. Los cojinetes abiertos de alta carga, la estructura de corte robusta y el tratamiento con carburo HET proporcionan altas tasas de penetración y una larga vida útil. Los conos ventilados (de 9» de diámetro y superiores) proporcionan limpieza adicional de los rodamientos y reducción del calor.',
+      'Los productos RIDGEBACK™ contienen cojinetes de mayor vida útil, protección adicional contra calibres y elementos de estructura de corte mejorados para su uso en aplicaciones de alta caída.'
     ], 
     mainImage: 'hero (1).webp',
     thumbnails: ['hero (1).webp'],
     contactLink: '#',
-    features: [],
-    downloads: []
+    features: [
+  {
+    title: 'RETENCIÓN DE BOQUILLAS DE UÑAS',
+    description: 'Incluso en las condiciones más abrasivas se elimina la pérdida de boquillas.'
+  }
+  ],
+    downloads: [
+  {
+    title: 'Catálogo de productos',
+    description: 'Descarga el catálogo en PDF.',
+    link: ''
+  }
+  ]
   };
 
   // obtener producto
